cfcbadbf35e2b4c203800a97c70eb6321c2a2c23
ezzedine elechi <ezzedine.elechi@gmail.com>
2022-01-05 14:24:04 +0100

    commit

--- modules/cours.js
+++ modules/cours.js
@@ -3,7 +3,10 @@ var Schema = mongoose.Schema;
 const courstSchema = mongoose.Schema({
     titre: { type: String, required: true },
     description: { type: String, required: true },
-    date: { type: String, required: false },
+    date: {
+        type: String, required: false,
+        enum: ['LUNDI', 'MARDI', 'MERCREDI', 'JEUDI', 'VENDREDI', 'SAMEDI', 'DIMANCHE', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+    },
     heure: { type: String, required: false },
 
 }, { versionKey: false, timestamps: true });
